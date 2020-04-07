871fea75efa1b95d4c261fe749b7d61ee655adab
lopeselio <lopeselio@gmail.com>
2020-04-07 14:46:33 +0530

    imports

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 // import 'App.css'
 import syled from 'styled-components'
+import AboutPage from './pages/AboutPage'
+import CartPage from './pages/CartPage'
+import ContactPage from './pages/ContactPage'
+import DefaultPage from './pages/DefaultPage'
+import HomePage from './pages/HomePage'
+import ProductsPage from './pages/ProductsPage'
+import SingleProductPage from './pages/SingleProductPage'
+
 
 function App () {
   return (
