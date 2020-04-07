28b83d65d62f00cc271d26c53c99182a95a83468
lopeselio <lopeselio@gmail.com>
2020-04-07 22:09:51 +0530

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,60 @@
 import React from 'react'
-import { FaBars, FaCart } from 'react-icons/fa'
+import { FaBars, FaCartPlus } from 'react-icons/fa'
 import styled from 'styled-components'
 import { ProductConsumer } from '../context'
-import logo from './../images/logo.svg'
-
+import logo from '../images/logo.svg'
 export default function Navbar () {
   return (
     <ProductConsumer>
       {value => {
-        const { cartItems, handleSidebar, handleCart = value }
-        return <NavWrapper>
-<div className='nav-center'>
-<FaBars className='nav-icon' onClick={handleSidebar} />
-<img src={logo} className alt='tech-store-logo' />
-<div className='nav-cart'>
-<FaCartPlus className='nav-icon' onClick={handleCart} />
-<div className='crt-items'>{cartItems}</div>
-</div>
-</div>
-      } 
-      }
+        const { cartItems, handleSidebar, handleCart } = value
+        return (
+          <NavWrapper>
+            <div className='nav-center'>
+              <FaBars className='nav-icon' onClick={handleSidebar} />
+              <img src={logo} alt='tech store logo' />
+              <div className='nav-cart'>
+                <FaCartPlus className='nav-icon' onClick={handleCart} />
+                <div className='cart-items'>{cartItems}</div>
+              </div>
+            </div>
+          </NavWrapper>
+        )
+      }}
     </ProductConsumer>
   )
 }
 
 const NavWrapper = styled.nav`
-
+  position: -webkit-sticky;
+  position: sticky;
+  top: 0;
+  width: 100%;
+  padding: 1rem 1.5rem;
+  background: var(--mainGrey);
+  border-bottom: 3px solid var(--primaryColor);
+  .nav-center {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    max-width: 1170px;
+    margin: 0 auto;
+  }
+  .nav-icon {
+    font-size: 1.5rem;
+    cursor: pointer;
+  }
+  .nav-cart {
+    position: relative;
+  }
+  .cart-items {
+    background: var(--primaryColor);
+    color: var(--mainWhite);
+    font-size: 0.85rem;
+    position: absolute;
+    top: -8px;
+    right: -8px;
+    padding: 0 5px;
+    border-radius: 50%;
+  }
 `
