3db61495013b0a19319124e8745c1e39c59d1a3a
lopeselio <lopeselio@gmail.com>
2020-04-07 21:58:37 +0530

    navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import { FaBars, FaCart } from 'react-icons/fa'
+import styled from 'styled-components'
+import { ProductConsumer } from '../context'
+import logo from './../images/logo.svg'
 
 export default function Navbar () {
   return (
-    <div>
-      Hello from Navbar
-    </div>
+    <ProductConsumer>
+      {value => {
+        const { cartItems, handleSidebar, handleCart = value }
+        return <NavWrapper>
+<div className='nav-center'>
+<FaBars className='nav-icon' onClick={handleSidebar} />
+<img src={logo} className alt='tech-store-logo' />
+<div className='nav-cart'>
+<FaCartPlus className='nav-icon' onClick={handleCart} />
+<div className='crt-items'>{cartItems}</div>
+</div>
+</div>
+      } 
+      }
+    </ProductConsumer>
   )
 }
+
+const NavWrapper = styled.nav`
+
+`
