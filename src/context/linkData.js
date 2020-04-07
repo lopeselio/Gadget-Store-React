49d48c035b2089fc5b144ff733f9e2d222c150c2
lopeselio <lopeselio@gmail.com>
2020-04-07 22:32:56 +0530

    creating link data

---
+++ src/context/linkData.js
@@ -0,0 +1,17 @@
+export const linkData = [
+  {
+    id: 1,
+    text: 'home',
+    path: '/'
+  },
+  {
+    id: 2,
+    text: 'about',
+    path: '/about'
+  },
+  {
+    id: 3,
+    text: 'products',
+    path: '/product'
+  }
+]
