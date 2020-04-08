fe6ecc791f43d5457ca5368fe83e9a22f9329f43
lopeselio <lopeselio@gmail.com>
2020-04-08 11:52:05 +0530

    hero component css

--- src/components/HeroPage.js
+++ src/components/HeroPage.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import styled from 'styled-components'
+import mainBcg from '../images/mainBcg.jpeg'
 
-export default function HeroPage() {
+export default function HeroPage ({ img, title, max, children }) {
   return (
-    <div>
-      Hello from hero component
-    </div>
+    <HeroWrapper max={max}>
+      <div className='banner'>
+        <h1 className='title'>{title}</h1>
+        {children}
+      </div>
+    </HeroWrapper>
   )
 }
+
+const HeroWrapper = styled.div`
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: ${props => (props.max ? '100vh' : '60vh')};
+
+
+`
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { ProductConsumer } from '../context/context'
+// import { ProductConsumer } from '../context/context'
 import HeroPage from '../components/HeroPage'
 import { Link } from 'react-router-dom'
 
 export default function HomePage () {
   return (
     <>
-      <HeroPage>
+      <HeroPage title='Awesome Gadgets'>
         <Link to='/products'>Our Products</Link>
       </HeroPage>
     </>
