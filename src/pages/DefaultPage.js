db447d0b499234e4c95b25f4c865fd8d7001e1a3
lopeselio <lopeselio@gmail.com>
2020-04-08 17:04:33 +0530

    Added default 404 Page

--- src/pages/DefaultPage.js
+++ src/pages/DefaultPage.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import HeroPage from '../components/HeroPage'
+import defaultBcg from '../images/defaultBcg.jpeg'
+import { Link } from 'react-router-dom'
 
 export default function DefaultPage () {
   return (
     <>
-      Hello from Default Page
+      <HeroPage img={defaultBcg} title={404} max>
+        <h2 className='text-uppercase'> Page Not Found</h2>
+        <Link to='/' className='main-link' style={{ marginTop: '2rem' }}>Return Home</Link>
+      </HeroPage>
     </>
   )
 }
