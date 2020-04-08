07b910781743e8049e4e92e148c961d1ebbf4ef6
lopeselio <lopeselio@gmail.com>
2020-04-08 17:21:04 +0530

    changes made to title, info and aboutPage sections

--- src/components/AboutPage/Info.js
+++ src/components/AboutPage/Info.js
@@ -1,9 +1,41 @@
 import React from 'react'
-
-export default function Info() {
+import Title from '../Title'
+import aboutBcg from '../../images/aboutBcg.jpeg'
+export default function Info () {
   return (
-    <div>
-      Hello from info!
-    </div>
+    <section className='py-5'>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-10 mx-auto col-md-6 my-3'>
+            <img
+              src={aboutBcg}
+              className='img-fluid img-thumbnail'
+              alt='about company'
+              style={{ background: 'var(--darkGrey)' }}
+            />
+          </div>
+          <div className='col-10 mx-auto col-md-6 my-3'>
+            <Title title='about us' />
+            <p className='text-lead text-muted my-3'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+              illo obcaecati non asperiores repellendus alias, nostrum aliquid
+              ad eaque ratione!
+            </p>
+            <p className='text-lead text-muted my-3'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+              illo obcaecati non asperiores repellendus alias, nostrum aliquid
+              ad eaque ratione!
+            </p>
+            <button
+              className='main-link'
+              type='button'
+              style={{ marginTop: '2rem' }}
+            >
+              more info
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import Info from '../components/AboutPage/Info'
+import Hero from '../components/Hero'
+import aboutBcg from '../images/aboutBcg.jpeg'
 export default function AboutPage () {
   return (
     <>
-      Hello from About Page
+      <Hero img={aboutBcg} />
+      <Info />
     </>
   )
 }
