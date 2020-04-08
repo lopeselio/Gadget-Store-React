21da16b919d1a857ee611f6a9ef56521941997ab
lopeselio <lopeselio@gmail.com>
2020-04-08 12:36:09 +0530

    Herowrapper CSS

--- src/components/HeroPage.js
+++ src/components/HeroPage.js
@@ -4,7 +4,7 @@ import mainBcg from '../images/mainBcg.jpeg'
 
 export default function HeroPage ({ img, title, max, children }) {
   return (
-    <HeroWrapper max={max}>
+    <HeroWrapper max={max} img={img}>
       <div className='banner'>
         <h1 className='title'>{title}</h1>
         {children}
@@ -19,6 +19,18 @@ const HeroWrapper = styled.div`
   align-items: center;
   justify-content: center;
   min-height: ${props => (props.max ? '100vh' : '60vh')};
-
+  color: var(--mainwhite);
+  background: linear-gradient(var(--primaryRGBA),var(--primaryRGBA)), url(${props => props => props.img}) center/cover no-repeat;
+  .title {
+    padding-top: 2rem;
+    font-size: 3.5rem;
+    text-shadow: 4px 2px 2px rgba(0, 0, 0, 0.3);
+    color: #fff;
+    text-transform: uppercase;
+    letter-spacing: var(--mainSpacing)
+  }
 
 `
+HeroPage.defaultProps = {
+  img: mainBcg
+}
