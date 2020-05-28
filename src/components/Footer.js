ba7e58eeb65e6326fd6aac9fc43f1368226b66fc
lopeselio <lopeselio@gmail.com>
2020-05-28 13:00:10 +0530

    added develioper assets

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import { ProductConsumer } from '../context'
+import devELIOper from '../images/develioper-04.png'
 export default function Footer () {
   return (
     <ProductConsumer>
@@ -8,19 +9,22 @@ export default function Footer () {
         return <FooterWrapper>
           <div className='container py-3'>
             <div className='row'>
-              <div className='col-md-6'>
+              <div className='col-md-4'>
                 <p className='text-capitalize'>
                 copyright &copy; Tech Store by devELIOper {new Date().getFullYear()}. all rights reserved{''}
                 </p>
               </div>
-              <div className='col-md-6 d-flex justify-content-around'>
+              <div className='col-md-4 d-flex justify-content-around'>
                 {value.socialIcons.map(item => (
                   <a href={item.path} key={item.id}>{item.icon}</a>
                 ))}
               </div>
+              <div className='col-md-4'>
+                <a href='https://www.github.com/lopeselio'><img className='pic' src={devELIOper} /></a>
+              </div>
             </div>
           </div>
-        </FooterWrapper>
+               </FooterWrapper>
       }}
     </ProductConsumer>
   )
@@ -36,5 +40,8 @@ const FooterWrapper = styled.footer`
   }
   .icon:hover {
     color: var(--primaryColor);
+  }
+  .pic {
+    height: 4rem;
   }
 `
