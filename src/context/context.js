6e9a0f635b59729d05221defe6aa8920a13115fb
lopeselio <lopeselio@gmail.com>
2020-06-16 13:51:16 +0530

    feat: localStorage

--- src/context/context.js
+++ src/context/context.js
@@ -55,7 +55,13 @@ class ProductProvider extends Component {
   };
   // get cart from local storage
   getStorageCart = () => {
-    return [];
+    let cart;
+    if (localStorage.getItem("cart")) {
+      cart = JSON.parse(localStorage.getItem("cart"));
+    } else {
+      cart = [];
+    }
+    return cart;
   };
   // get product from local storage
   getStorageProduct = () => {
@@ -93,7 +99,9 @@ class ProductProvider extends Component {
     });
   };
   // sync storage
-  syncStorage = () => {};
+  syncStorage = () => {
+    localStorage.setItem("cart", JSON.stringify(this.state.cart));
+  };
   //add to cart
   addToCart = id => {
     let tempCart = [...this.state.cart];
