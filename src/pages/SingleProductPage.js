c2f73ed3cd2d689439a035eaee41cf972562c65b
lopeselio <lopeselio@gmail.com>
2020-07-02 02:28:59 +0530

    chnages

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Hero from '../components/HeroPage'
+import Hero from '../components/Hero'
 import singleProductImg from '../images/singleProductBcg.jpeg'
 import { ProductConsumer } from '../context'
 export default function SingleProductPage () {
