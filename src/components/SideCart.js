638f9af209fb3b523f07046370a7894e684747ce
lopeselio <lopeselio@gmail.com>
2020-07-02 02:34:23 +0530

    sideCart display

--- src/components/SideCart.js
+++ src/components/SideCart.js
@@ -1,15 +1,42 @@
 import React from 'react'
 import { ProductConsumer } from '../context'
 import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 export default function SideCart () {
   return (
     <ProductConsumer>
       {value => {
-        // eslint-disable-next-line no-unused-vars
-        const { cartOpen, closeCart, cart } = value
+        const { cartOpen, closeCart, cart, cartTotal } = value
         return (
           <CartWrapper show={cartOpen} onClick={closeCart}>
-            <p>cart items</p>
+            <ul>
+              {cart.map(item => {
+                return (
+                  <li key={item.id} className='cart-item mb-4'>
+                    <img
+                      width='35'
+                      src={`../${item.image}`}
+                      // src={item.image}
+                      alt='cart item'
+                    />
+                    <div className='mt-3'>
+                      <h6 className='text-uppercase'>{item.title}</h6>
+                      <h6 className='text-title text-capitalize'>
+                        amount : {item.count}
+                      </h6>
+                    </div>
+                  </li>
+                )
+              })}
+            </ul>
+            <h4 className='text-capitalize text-main'>
+              cart total : ${cartTotal}
+            </h4>
+            <div className='text-center my-5'>
+              <Link to='/cart' className='main-link'>
+                cart page
+              </Link>
+            </div>
           </CartWrapper>
         )
       }}
@@ -30,5 +57,13 @@ const CartWrapper = styled.div`
   transition: var(--mainTransition);
   @media (min-width: 576px) {
     width: 20rem;
+  }
+  overflow: scroll;
+  padding: 2rem;
+  ul {
+    padding: 0 !important;
+  }
+  .cart-item {
+    list-style-type: none;
   }
 `
