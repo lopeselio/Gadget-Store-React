88d36e27e164d42ee3ce0275289ea5c3bb5bb7dd
lopeselio <lopeselio@gmail.com>
2020-07-02 02:04:04 +0530

    viewing a single product

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -1,9 +1,74 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+import Hero from '../components/Hero'
+import singleProductImg from '../images/singleProductBcg.jpeg'
+import { ProductConsumer } from '../context'
 export default function SingleProductPage () {
   return (
     <>
-      Hello from Single products Page
+      <Hero img={singleProductImg} title='single product' />
+      <ProductConsumer>
+        {value => {
+          const { singleProduct, addToCart, loading } = value
+
+          if (loading) {
+            console.log('hello from loading')
+            return <h1>product loading....</h1>
+          }
+          const {
+            company,
+            description,
+            id,
+            price,
+            title,
+            image
+          } = singleProduct
+          return (
+            <section className='py-5'>
+              <div className='container'>
+                <div className='row'>
+                  <div className='col-10 mx-auto col-sm-8 col-md-6 my-3'>
+                    <img
+                      src={`../${image}`}
+                      // src={image}
+                      alt='single product'
+                      className='img-fluid'
+                    />
+                  </div>
+                  <div className='col-10 mx-auto col-sm-8 col-md-6 my-3'>
+                    <h5 className='text-title mb-4'>model :{title} </h5>
+                    <h5 className='text-capitalize text-muted mb-4'>
+                      company : {company}
+                    </h5>
+                    <h5 className='text-main text-capitalize mb-4'>
+                      price : ${price}
+                    </h5>
+                    <p className='text-capitalize text-title mt-3'>
+                      some info about product :
+                    </p>
+                    <p>{description}</p>
+                    <button
+                      type='button'
+                      className='main-link'
+                      style={{ margin: '0.75rem' }}
+                      onClick={() => addToCart(id)}
+                    >
+                      add to cart
+                    </button>
+                    <Link
+                      to='/products'
+                      className='main-link'
+                      style={{ margin: '0.75rem' }}
+                    >
+                      back to products
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </section>
+          )
+        }}
+      </ProductConsumer>
     </>
   )
 }
