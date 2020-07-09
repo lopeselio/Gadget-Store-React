7bbf8e10dc0fd97e9af514096c14c5e0239562ce
lopeselio <lopeselio@gmail.com>
2020-07-10 00:00:19 +0530

    cart totals

--- src/components/CartPage/CartTotals.js
+++ src/components/CartPage/CartTotals.js
@@ -3,8 +3,22 @@ import { ProductConsumer } from '../../context'
 
 export default function CartTotals () {
   return (
-    <div>
-      CartTotals
+    <div className='container'>
+      <div className='row'>
+        <ProductConsumer>
+          {value => {
+            const { clearCart, cartSubTotal, cartTax, cartTotal } = value
+            return (
+              <div className='col text-title text-center my-4'>
+                <button className='btn btn-outline-danger text-capitalize mb-4' onClick={clearCart}>Clear Cart</button> 
+                <h3>Subtotal : ${cartSubTotal}</h3>
+                <h3>Taxes: ${cartTax}</h3>
+                <h3>Total: ${cartTotal}</h3>
+              </div>
+            )
+          }}
+        </ProductConsumer>
+      </div>
     </div>
   )
 }
