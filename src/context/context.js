84fb48fe13cd286df35dc3994209a9b1ca27f303
lopeselio <lopeselio@gmail.com>
2020-07-10 23:42:04 +0530

    added set data structure for filter product

--- src/context/context.js
+++ src/context/context.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { linkData } from "./linkData";
 import { socialData } from "./socialData";
-import { items } from "./data/productData";
+import { items } from "./productData";
 const ProductContext = React.createContext();
 //Provider
 //Consumer
