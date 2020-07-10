8e76426dc404cd54fb0e0bc393819bcabae7ce52
lopeselio <lopeselio@gmail.com>
2020-07-10 13:45:47 +0530

    chenged products

--- src/components/ProductsPage/Products.js
+++ src/components/ProductsPage/Products.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { ProductConsumer } from '../../context'
 import Title from '../Title'
 import Product from '../Product'
-
+import ProductFilter from './ProductFilter'
 export default function Products () {
   return (
     <ProductConsumer>
@@ -12,12 +12,28 @@ export default function Products () {
           <section className='py-5'>
             <div className='container'>
               {/* title */}
-              <Title center title='Our Products' />
+              <Title center title='our products' />
+              {/* Product filter */}
+              <ProductFilter />
+              {/* total count */}
+              <div className='row'>
+                <div className='col-10 mx-auto'>
+                  <h6 className='text-title'>
+                    total products : {filteredProducts.length}
+                  </h6>
+                </div>
+              </div>
               {/* products */}
               <div className='row py-5'>
-                {filteredProducts.map(product => {
-                  return <Product key={product.id} product={product} />
-                })}
+                {filteredProducts.length === 0 ? (
+                  <div className='col text-title text-center'>
+                    sorry, no items matched your search
+                  </div>
+                ) : (
+                  filteredProducts.map(product => {
+                    return <Product key={product.id} product={product} />
+                  })
+                )}
               </div>
             </div>
           </section>
